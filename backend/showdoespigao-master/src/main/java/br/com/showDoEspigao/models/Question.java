42d7ac258bb66c2210bd9ec74062481a2d98f6af
MateusLopesNunes <mateuslopesnunes@outlook.com>
2022-09-20 21:39:02 -0300

    h2

--- backend/showdoespigao-master/src/main/java/br/com/showDoEspigao/models/Question.java
+++ backend/showdoespigao-master/src/main/java/br/com/showDoEspigao/models/Question.java
@@ -1,14 +1,14 @@
 package br.com.showDoEspigao.models;
 
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-//@Entity
+@Entity
 public class Question {
 
-   // @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
