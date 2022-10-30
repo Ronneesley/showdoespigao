93b99c04bdf3075c76b0d81f5a0edbaed22c02b6
Paulo Henrique <paulo.alves.123.ph@gmail.com>
2022-10-30 19:01:03 -0300

    Adicionando tela de premio atual

---
+++ frontend/showDoEspigao/app/src/main/java/com/example/showdoespigao/Premioatual_Activity.java
@@ -0,0 +1,14 @@
+package com.example.premio_atual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Premioatual_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.premio_atual);
+    }
+}
