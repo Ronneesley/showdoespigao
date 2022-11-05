2251753d0cafedcf115d51ae854315a903437e90
Matheus Mederado <87718854+MathMedrado@users.noreply.github.com>
2022-11-05 12:17:00 -0300

    Update roleta.java
    resolvendo o merge na mão

--- backend/roleta.java
+++ backend/roleta.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Random;
 
 public class Roleta {
@@ -46,52 +45,4 @@ public class Roleta {
         r.sortear();
     }
 }
-=======
-import java.util.Random;
 
-public class Roleta {
-    public double opA;
-    public double opB;
-    public double opC;
-    public double opD;
-    public void setA(double a) { 
-        this.opA = a; 
-    }
-    public void setB(double b) { 
-        this.opB = b; 
-    }
-    public void setC(double c) {
-        this.opC = c;
-    }
-    public void setD(double d) {
-        this.opD = d;
-    }
-    
-    public int sortear() {
-        int sorteado = 0;
-        Random rand = new Random();
-        int[] possibilidades = {1, 2, 3, 4};
-        double[] probabilidades = {opA, opB, opC, opD};
-        double total = 0;
-        double chanceSorteada = rand.nextDouble();
-        for (int j = 0; j < possibilidades.length; j++) {
-            total += probabilidades[j];
-            if (chanceSorteada <= total) {
-                System.out.println(possibilidades[j]);
-                sorteado = possibilidades[j];
-                break;
-            }
-        }
-        return sorteado;
-    }
-    public static void main(String[] args) {
-        Roleta r = new Roleta();
-        r.setA(0.10);
-        r.setB(0.70);
-        r.setC(0.10);
-        r.setD(0.10);
-        
-        r.sortear();
-    }
-}
->>>>>>> 08e621a8414582da39e030402161d0bdc6d01c64
